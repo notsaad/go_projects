f09430b9ee78586a97e2e76128e92d5decae8405
notsaad <saadmazharr@gmail.com>
2025-04-03 23:15:02 -0700

    stuff
    Signed-off-by: notsaad <saadmazharr@gmail.com>

--- cli_todo/main.go
+++ cli_todo/main.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
-func main() {
-  // first parameter - name of the flag (what users type on the cli)
-  // second parameter - default value that is used if the user doesnt specify a value
-  // third parameter - usage string, appears when someone uses the help flag
-  count := flag.Int("count", 1, "Number of times to print Hello World")
-
-  flag.Parse()
-
-  fmt.Println("count has value: ", *count)
-  // cli input validation
-  if *count < 1 {
-    os.Exit(1)
-  }
-
-  for i := 0; i < *count; i++ {
-    fmt.Println("hello!")
-  }
-
+type Task struct {
+	ID     string `json:"id"`
+	text   string `json:"text"`
+	Status bool   `json:"status"`
 }
 
+func main() {
+	// first parameter - name of the flag (what users type on the cli)
+	// second parameter - default value that is used if the user doesnt specify a value
+	// third parameter - usage string, appears when someone uses the help flag
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'add' or 'list' subcommand")
+		os.Exit(1) // general error code
+	}
+
+	switch os.Args[1] {
+	case "add":
+		addCmd.Parse(os.Args[2:])
+		fmt.Println("Adding a task", *addCmd)
+	case "list":
+		fmt.Println("Listing tasks")
+	}
+
+}

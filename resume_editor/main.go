330e7b85732f331f9266fbaaf25a4268686d6608
notsaad <saadmazharr@gmail.com>
2025-05-01 22:36:38 -0700

    latex compilation
    Signed-off-by: notsaad <saadmazharr@gmail.com>

--- resume_editor/main.go
+++ resume_editor/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello, Resume Editor!")
+	var fileName string
+	fmt.Print("Enter the LaTeX file name (without extension): ")
+	fmt.Scanln(&fileName)
+
+	fmt.Println("Compiling LaTeX file:", fileName+".tex")
+
+	cmd := exec.Command("pdflatex", fileName)
+	err := cmd.Run() // Run waits for the command to finish
+	// can also use cmd.CombinedOutput() to get the output of the command
+	if err != nil {
+		fmt.Println("Error compiling LaTeX code:", err)
+		return
+	}
+
+	// Run another command after pdflatex is done
+	secondCmd := exec.Command("rm", fileName+".log", fileName+".aux", fileName+".out")
+	err = secondCmd.Run()
+	if err != nil {
+		fmt.Println("Error running second command:", err)
+		return
+	}
+
+	// Print the output
+	fmt.Println("PDF compiled successfully!")
 }
